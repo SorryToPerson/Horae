168b81e01a5b4cf4ea5f9960d1b71454307fe0d2
xulibang <xulibangyaojingjing@gmail.com>
2025-05-27 14:12:38 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Pagination } from '@mui/material';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import './App.css';
 
 function App() {
   return (
-    <main className="flex flex-col gap-4 justify-center items-center h-screen">
-      <div className="text-3xl font-bold underline">Welcome to Tauri + React</div>
-      <Pagination count={10} variant="outlined" color="secondary" />
+    <main className="h-screen">
+      <DateCalendar className="h-full" />
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <App />
+    </LocalizationProvider>
   </React.StrictMode>,
 );
